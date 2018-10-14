ee2481cd73153c51ac258448b6a33b9829d7b03d
Jasper <pannicke.jasper@gmail.com>
2018-10-15 00:29:55 +0200

    Add BookList Component

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import BookList from './components/BookList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <h1>GraphQL Demo</h1>
+        <BookList></BookList>
       </div>
     );
   }
