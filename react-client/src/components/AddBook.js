935d86444567abf486469f191057854acc1dd666
Jasper Pannicke <Jasper.Pannicke@deutschebahn.com>
2018-10-18 10:59:11 +0200

    Fix issue

--- react-client/src/components/AddBook.js
+++ react-client/src/components/AddBook.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
-import { getBooksQuery } from '../queries/queries';
+import { getAuthorsQuery } from '../queries/queries';
 
 class AddBook extends Component {
   displayAuthors() {
