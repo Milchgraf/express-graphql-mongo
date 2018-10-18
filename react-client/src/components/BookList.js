070ccdfaae161bbd0f77259efbdcf07118b3cefe
Jasper Pannicke <Jasper.Pannicke@deutschebahn.com>
2018-10-18 12:07:06 +0200

    Add BokkDetails Component

--- react-client/src/components/BookList.js
+++ react-client/src/components/BookList.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { getBooksQuery } from '../queries/queries';
+import BookDetails from './BookDetails';
 
 class BookList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null
+    }
+  }
+
   displayBooks() {
     const data = this.props.data;
 
@@ -13,7 +21,7 @@ class BookList extends Component {
     } else {
       return data.books.map(book => {
         return (
-          <li key={ book.id }>{ book.name } ({ book.genre })</li>
+          <li key={ book.id } onClick={ (event) =>  this.setState({ selected: book.id }) }>{ book.name }</li>
         );
       });
     }
@@ -25,6 +33,7 @@ class BookList extends Component {
         <ul id="book-list">
           { this.displayBooks() }
         </ul>
+        <BookDetails bookId={ this.state.selected }></BookDetails>
       </div>
     );
   }
